53a2976a0eb70f82485bdaf677fa2a7cf437e143
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-09-20 22:29:15 +0300

    built my second API and passed all the tests

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
+from sqlalchemy import desc
 
 from models import db, Bakery, BakedGood
 
@@ -20,19 +21,64 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+    bakeries_list =[]
+    for bakery in bakeries:
+        bakery_dict = {
+            'id':bakery.id,
+            'name': bakery.name,
+            'created_at':bakery.created_at,
+            'updated_at':bakery.updated_at
+        }
+        bakeries_list.append(bakery_dict)
+
+    return make_response(jsonify(bakeries_list),200)
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter_by(id=id).first()
+    bakery_data = bakery.to_dict()
+    print(bakery_data)
+    # bakery_data = {
+    #     'name' : bakery.name,
+    #     'created_at' : bakery.created_at,
+    #     'updated_at':bakery.updated_at
+    # }
+ 
+    return make_response(jsonify(bakery_data,))
+
+
+ 
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+     baked_goods = BakedGood.query.order_by(desc(BakedGood.price)).all()
+     baked_goods_list = []
+     for bakery_good in baked_goods:
+        baked_goods_dict = {
+            'id':bakery_good.id,
+            'name':bakery_good.name,
+            'created_at':bakery_good.created_at,
+            'updated_at':bakery_good.updated_at,
+            'bakery_id':bakery_good.bakery_id,
+            'price':bakery_good.price
+        }
+        baked_goods_list.append(baked_goods_dict)
+     return make_response(jsonify(baked_goods_list),200)
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive = BakedGood.query.order_by(desc(BakedGood.price)).limit(1).first()
+    most_expensive_dict = {
+
+            'id':most_expensive.id,
+            'name':most_expensive.name,
+            'price':most_expensive.price,
+            'created_at':most_expensive.created_at,
+            'updated_at':most_expensive.updated_at,
+            'bakery_id':most_expensive.bakery_id,
+    }
+    return make_response(most_expensive_dict,200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
